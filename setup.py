9284215f7db10440d8afda7f5423a73b0048310e
Richard Rogers <richard@whylabs.ai>
2023-06-20 16:37:54 -0700

    fork

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-VERSION = '0.0.1'
-DESCRIPTION = "PyBloom: A Probabilistic data structure"
+VERSION = '0.1.0'
+DESCRIPTION = "Fork of PyBloom: A Probabilistic data structure"
 LONG_DESCRIPTION = """
+Minor fork of pybloom to get it working in a modern Python enviornment.
 pybloom is a Python implementation of the bloom filter probabilistic data
 structure. The module also provides a Scalable Bloom Filter that allows a
 bloom filter to grow without knowing the original set size.
@@ -19,10 +20,10 @@ setup(name="pybloom3",
       ],
       author="Jay Baird",
       author_email="jay.baird@me.com",
-      url="http://github.com/hexmagic/python-bloomfilter/",
+      url="http://github.com/whylabs/whylogs-bloom/",
       license="MIT License",
       platforms=['any'],
       test_suite="pybloom.tests",
       zip_safe=True,
       install_requires=['bitarray>=0.3.4'],
-      packages=['pybloom'])
+      packages=['whylogs-bloom'])
